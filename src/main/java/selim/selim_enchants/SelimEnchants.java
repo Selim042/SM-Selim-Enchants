d56e88aed1bfa42b10a11dec939f603d494d0090
Myles Selim <Selim042@users.noreply.github.com>
2018-09-10 17:25:20 -0500

    Version bump

--- src/main/java/selim/selim_enchants/SelimEnchants.java
+++ src/main/java/selim/selim_enchants/SelimEnchants.java
@@ -13,7 +13,7 @@ public class SelimEnchants {
 
 	public static final String MOD_ID = "selim_enchants";
 	public static final String NAME = "Selim's Enchants";
-	public static final String VERSION = "2.0.1";
+	public static final String VERSION = "2.0.2";
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
