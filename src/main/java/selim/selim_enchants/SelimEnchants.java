a431897b1c02f8170cb42977a40432fb639445f4
Selim <042Selim@gmail.com>
2023-06-07 21:01:40 -0400

    Fix locations of jsons

--- src/main/java/selim/selim_enchants/SelimEnchants.java
+++ src/main/java/selim/selim_enchants/SelimEnchants.java
@@ -31,7 +31,7 @@ public class SelimEnchants {
 
 	public static final String MOD_ID = "selim_enchants";
 	public static final String NAME = "Selim's Enchants";
-	public static final String VERSION = "3.0.0";
+	public static final String VERSION = "3.0.1";
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
